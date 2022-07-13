585aa3ecc43906128dce513031dfa284d1029358
snqig <55125005+snqig@users.noreply.github.com>
2022-07-13 14:36:26 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "c1bca1b2-5575-4c62-87d0-322267857d34",
                         "alterId": 0
                     }
                 ],
